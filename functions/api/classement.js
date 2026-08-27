073742ed713cce3eb43d4ad068dd91e4c063e72c
nolananm <antomicha.nl@gmail.com>
2026-08-27 12:35:01 +0200

    Update classement.js

--- functions/api/classement.js
+++ functions/api/classement.js
@@ -3,7 +3,6 @@ export async function onRequest(context) {
     const url = 'https://api.football-data.org/v4/competitions/FL1/standings';
 
     try {
-        // C'est Cloudflare qui fait la requête, donc aucun blocage CORS !
         const response = await fetch(url, {
             headers: {
                 'X-Auth-Token': apiToken
@@ -12,7 +11,6 @@ export async function onRequest(context) {
         
         const data = await response.json();
 
-        // On renvoie les données à ton site web
         return new Response(JSON.stringify(data), {
             headers: {
                 'Content-Type': 'application/json',
